f81c0be7d0d418a0d370365d6759e08d11369bfa
James <yangj_2003@hotmail.com>
2017-03-11 22:02:41 -0800

    delete the error version

--- libplatform/inc/base64.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BASE64_H__
-#define __BASE64_H__
-
-extern int base64decode(void *dst,char *src,int maxlen);
-extern void base64encode(unsigned char *from, char *to, int len);
-
-#endif /*__BASE64_H__*/
-
